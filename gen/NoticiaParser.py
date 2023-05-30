996fc650cc5903525e6f6f3e6567d7bd0191cad5
Rian Galatas <rian.team22@gmail.com>
2023-05-30 18:33:10 -0300

    mudanças

--- gen/NoticiaParser.py
+++ gen/NoticiaParser.py
@@ -1,4 +1,4 @@
-# Generated from C:/Users/davij/PycharmProjects/ExemploAntlr\Noticia.g4 by ANTLR 4.12.0
+# Generated from C:/Users/ryant/OneDrive/Área de Trabalho/2023.1/COMPILADORES/LexiconAnalyzer\Noticia.g4 by ANTLR 4.12.0
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -10,14 +10,17 @@ else:
 
 def serializedATN():
     return [
-        4,1,4,28,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,2,4,7,4,1,0,1,0,4,0,13,
-        8,0,11,0,12,0,14,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,3,1,3,1,4,1,4,1,4,
-        0,0,5,0,2,4,6,8,0,0,23,0,10,1,0,0,0,2,16,1,0,0,0,4,20,1,0,0,0,6,
-        23,1,0,0,0,8,25,1,0,0,0,10,12,5,1,0,0,11,13,3,2,1,0,12,11,1,0,0,
-        0,13,14,1,0,0,0,14,12,1,0,0,0,14,15,1,0,0,0,15,1,1,0,0,0,16,17,3,
-        4,2,0,17,18,5,1,0,0,18,19,3,8,4,0,19,3,1,0,0,0,20,21,5,2,0,0,21,
-        22,5,1,0,0,22,5,1,0,0,0,23,24,5,2,0,0,24,7,1,0,0,0,25,26,5,3,0,0,
-        26,9,1,0,0,0,1,14
+        4,1,3,35,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,1,0,1,0,1,1,1,1,1,1,1,2,
+        1,2,1,2,5,2,17,8,2,10,2,12,2,20,9,2,1,3,1,3,1,3,5,3,25,8,3,10,3,
+        12,3,28,9,3,5,3,30,8,3,10,3,12,3,33,9,3,1,3,0,0,4,0,2,4,6,0,0,33,
+        0,8,1,0,0,0,2,10,1,0,0,0,4,13,1,0,0,0,6,31,1,0,0,0,8,9,3,2,1,0,9,
+        1,1,0,0,0,10,11,3,4,2,0,11,12,3,6,3,0,12,3,1,0,0,0,13,18,5,2,0,0,
+        14,15,5,1,0,0,15,17,5,2,0,0,16,14,1,0,0,0,17,20,1,0,0,0,18,16,1,
+        0,0,0,18,19,1,0,0,0,19,5,1,0,0,0,20,18,1,0,0,0,21,26,5,2,0,0,22,
+        23,5,1,0,0,23,25,5,2,0,0,24,22,1,0,0,0,25,28,1,0,0,0,26,24,1,0,0,
+        0,26,27,1,0,0,0,27,30,1,0,0,0,28,26,1,0,0,0,29,21,1,0,0,0,30,33,
+        1,0,0,0,31,29,1,0,0,0,31,32,1,0,0,0,32,7,1,0,0,0,33,31,1,0,0,0,3,
+        18,26,31
     ]
 
 class NoticiaParser ( Parser ):
@@ -30,24 +33,21 @@ class NoticiaParser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [  ]
+    literalNames = [ "<INVALID>", "' '" ]
 
-    symbolicNames = [ "<INVALID>", "NEWLINE", "TITLE_TEXT", "BODY_TEXT", 
-                      "INFO" ]
+    symbolicNames = [ "<INVALID>", "<INVALID>", "WORD", "WS" ]
 
     RULE_ini = 0
     RULE_article = 1
     RULE_title_content = 2
-    RULE_title = 3
-    RULE_body = 4
+    RULE_body_content = 3
 
-    ruleNames =  [ "ini", "article", "title_content", "title", "body" ]
+    ruleNames =  [ "ini", "article", "title_content", "body_content" ]
 
     EOF = Token.EOF
-    NEWLINE=1
-    TITLE_TEXT=2
-    BODY_TEXT=3
-    INFO=4
+    T__0=1
+    WORD=2
+    WS=3
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
@@ -65,14 +65,8 @@ class NoticiaParser ( Parser ):
             super().__init__(parent, invokingState)
             self.parser = parser
 
-        def NEWLINE(self):
-            return self.getToken(NoticiaParser.NEWLINE, 0)
-
-        def article(self, i:int=None):
-            if i is None:
-                return self.getTypedRuleContexts(NoticiaParser.ArticleContext)
-            else:
-                return self.getTypedRuleContext(NoticiaParser.ArticleContext,i)
+        def article(self):
+            return self.getTypedRuleContext(NoticiaParser.ArticleContext,0)
 
 
         def getRuleIndex(self):
@@ -99,23 +93,10 @@ class NoticiaParser ( Parser ):
 
         localctx = NoticiaParser.IniContext(self, self._ctx, self.state)
         self.enterRule(localctx, 0, self.RULE_ini)
-        self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 10
-            self.match(NoticiaParser.NEWLINE)
-            self.state = 12 
-            self._errHandler.sync(self)
-            _la = self._input.LA(1)
-            while True:
-                self.state = 11
-                self.article()
-                self.state = 14 
-                self._errHandler.sync(self)
-                _la = self._input.LA(1)
-                if not (_la==2):
-                    break
-
+            self.state = 8
+            self.article()
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -136,11 +117,8 @@ class NoticiaParser ( Parser ):
             return self.getTypedRuleContext(NoticiaParser.Title_contentContext,0)
 
 
-        def NEWLINE(self):
-            return self.getToken(NoticiaParser.NEWLINE, 0)
-
-        def body(self):
-            return self.getTypedRuleContext(NoticiaParser.BodyContext,0)
+        def body_content(self):
+            return self.getTypedRuleContext(NoticiaParser.Body_contentContext,0)
 
 
         def getRuleIndex(self):
@@ -169,12 +147,10 @@ class NoticiaParser ( Parser ):
         self.enterRule(localctx, 2, self.RULE_article)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 16
+            self.state = 10
             self.title_content()
-            self.state = 17
-            self.match(NoticiaParser.NEWLINE)
-            self.state = 18
-            self.body()
+            self.state = 11
+            self.body_content()
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -191,11 +167,11 @@ class NoticiaParser ( Parser ):
             super().__init__(parent, invokingState)
             self.parser = parser
 
-        def TITLE_TEXT(self):
-            return self.getToken(NoticiaParser.TITLE_TEXT, 0)
-
-        def NEWLINE(self):
-            return self.getToken(NoticiaParser.NEWLINE, 0)
+        def WORD(self, i:int=None):
+            if i is None:
+                return self.getTokens(NoticiaParser.WORD)
+            else:
+                return self.getToken(NoticiaParser.WORD, i)
 
         def getRuleIndex(self):
             return NoticiaParser.RULE_title_content
@@ -221,12 +197,23 @@ class NoticiaParser ( Parser ):
 
         localctx = NoticiaParser.Title_contentContext(self, self._ctx, self.state)
         self.enterRule(localctx, 4, self.RULE_title_content)
+        self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 20
-            self.match(NoticiaParser.TITLE_TEXT)
-            self.state = 21
-            self.match(NoticiaParser.NEWLINE)
+            self.state = 13
+            self.match(NoticiaParser.WORD)
+            self.state = 18
+            self._errHandler.sync(self)
+            _la = self._input.LA(1)
+            while _la==1:
+                self.state = 14
+                self.match(NoticiaParser.T__0)
+                self.state = 15
+                self.match(NoticiaParser.WORD)
+                self.state = 20
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -236,91 +223,68 @@ class NoticiaParser ( Parser ):
         return localctx
 
 
-    class TitleContext(ParserRuleContext):
+    class Body_contentContext(ParserRuleContext):
         __slots__ = 'parser'
 
         def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
             super().__init__(parent, invokingState)
             self.parser = parser
 
-        def TITLE_TEXT(self):
-            return self.getToken(NoticiaParser.TITLE_TEXT, 0)
+        def WORD(self, i:int=None):
+            if i is None:
+                return self.getTokens(NoticiaParser.WORD)
+            else:
+                return self.getToken(NoticiaParser.WORD, i)
 
         def getRuleIndex(self):
-            return NoticiaParser.RULE_title
+            return NoticiaParser.RULE_body_content
 
         def enterRule(self, listener:ParseTreeListener):
-            if hasattr( listener, "enterTitle" ):
-                listener.enterTitle(self)
+            if hasattr( listener, "enterBody_content" ):
+                listener.enterBody_content(self)
 
         def exitRule(self, listener:ParseTreeListener):
-            if hasattr( listener, "exitTitle" ):
-                listener.exitTitle(self)
+            if hasattr( listener, "exitBody_content" ):
+                listener.exitBody_content(self)
 
         def accept(self, visitor:ParseTreeVisitor):
-            if hasattr( visitor, "visitTitle" ):
-                return visitor.visitTitle(self)
+            if hasattr( visitor, "visitBody_content" ):
+                return visitor.visitBody_content(self)
             else:
                 return visitor.visitChildren(self)
 
 
 
 
-    def title(self):
+    def body_content(self):
 
-        localctx = NoticiaParser.TitleContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 6, self.RULE_title)
+        localctx = NoticiaParser.Body_contentContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 6, self.RULE_body_content)
+        self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 23
-            self.match(NoticiaParser.TITLE_TEXT)
-        except RecognitionException as re:
-            localctx.exception = re
-            self._errHandler.reportError(self, re)
-            self._errHandler.recover(self, re)
-        finally:
-            self.exitRule()
-        return localctx
+            self.state = 31
+            self._errHandler.sync(self)
+            _la = self._input.LA(1)
+            while _la==2:
+                self.state = 21
+                self.match(NoticiaParser.WORD)
+                self.state = 26
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+                while _la==1:
+                    self.state = 22
+                    self.match(NoticiaParser.T__0)
+                    self.state = 23
+                    self.match(NoticiaParser.WORD)
+                    self.state = 28
+                    self._errHandler.sync(self)
+                    _la = self._input.LA(1)
 
+                self.state = 33
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
 
-    class BodyContext(ParserRuleContext):
-        __slots__ = 'parser'
-
-        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
-            super().__init__(parent, invokingState)
-            self.parser = parser
-
-        def BODY_TEXT(self):
-            return self.getToken(NoticiaParser.BODY_TEXT, 0)
-
-        def getRuleIndex(self):
-            return NoticiaParser.RULE_body
-
-        def enterRule(self, listener:ParseTreeListener):
-            if hasattr( listener, "enterBody" ):
-                listener.enterBody(self)
-
-        def exitRule(self, listener:ParseTreeListener):
-            if hasattr( listener, "exitBody" ):
-                listener.exitBody(self)
-
-        def accept(self, visitor:ParseTreeVisitor):
-            if hasattr( visitor, "visitBody" ):
-                return visitor.visitBody(self)
-            else:
-                return visitor.visitChildren(self)
-
-
-
-
-    def body(self):
-
-        localctx = NoticiaParser.BodyContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 8, self.RULE_body)
-        try:
-            self.enterOuterAlt(localctx, 1)
-            self.state = 25
-            self.match(NoticiaParser.BODY_TEXT)
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
